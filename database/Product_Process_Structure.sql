f6f189aee6a33e871092d5a48a877cc12ae54cfb
aandreou <aandreou25@gmail.com>
2025-07-02 20:36:32 +0100

    SQL file updates

---
+++ database/Product_Process_Structure.sql
@@ -0,0 +1,21 @@
+== Weekly 
+== Run at any time and ok to run multiple times if needed. It will overide data for the week.
+
+1. Clean sales Database
+clean_sales.sql
+Table: sales
+
+2. Refresh Sales Data
+refresh_performance.sql
+Table: performance
+Table: groupid_performance
+
+3. Snapshot
+weekly_snapshot.sql
+Table: groupid_performance_week
+
+
+
+--- Weekly meeting for progress update
+--- Bi-Weekly meeting for clearance check
+
