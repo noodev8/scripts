d5ce517ad73c3a92d3d384809bf7e2557a49e21f
noodev8 <aandreou25@gmail.com>
2025-07-05 19:11:07 +0000

    Add database backup script

---
+++ pg_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BACKUP_DIR="/apps/scripts/database"
+DATE=$(date +%Y-%m-%d)
+
+mkdir -p "$BACKUP_DIR"
+
+# Explicitly list your production databases
+DBS=("brookfield_prod" "splitleague_prod")
+
+for DB in "${DBS[@]}"; do
+    echo "Backing up database: $DB"
+    sudo -u postgres pg_dump -Fc "$DB" > "$BACKUP_DIR/${DB}_$DATE.dump"
+done
+
+# Compress all .dump files
+gzip -f $BACKUP_DIR/*.dump
+
+# Delete backups older than 7 days
+find "$BACKUP_DIR" -type f -mtime +7 -delete
